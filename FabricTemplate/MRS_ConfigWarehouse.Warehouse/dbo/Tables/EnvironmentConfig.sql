afebd86858e4e6f00314ad87dae74a7e3c47ff68
ritikbansalhcstcs21-creator <ritikbansal.hcst.cs21@sgei.org>
2025-11-15 18:54:49 +0530

    Delete FabricTemplate directory

--- FabricTemplate/MRS_ConfigWarehouse.Warehouse/dbo/Tables/EnvironmentConfig.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE [dbo].[EnvironmentConfig] (
-
-	[WorkspaceID] varchar(200) NULL, 
-	[WorkspaceName] varchar(200) NULL, 
-	[SqlConnectionID] varchar(200) NULL, 
-	[SqlServerName] varchar(300) NULL, 
-	[SqlDatabaseName] varchar(300) NULL, 
-	[TableName] varchar(200) NULL
-);
--- FabricTemplate/MRS_DW.Warehouse/dbo/Tables/SalesData.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE [dbo].[SalesData] (
-
-	[SaleID] int NULL, 
-	[Product] varchar(50) NULL, 
-	[Quantity] int NULL, 
-	[Amount] int NULL
-);
